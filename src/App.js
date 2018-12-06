2f10c30d272782aef73dd33f8d9f2dd0f038409d
serpient <littlegangwolf@gmail.com>
2018-12-06 13:11:49 -0800

    add weekly checkin example

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import * as React from 'react';
 import { Switch, Route } from "react-router-dom";
 import VoyageApplication from './components/VoyageApplication';
 import Missing404Page from './components/404/404';
+import WeeklyCheckin from './components/WeeklyCheckin';
 class App extends React.Component {
   render() {
     return (
       <div className="App">
         <Switch>
           <Route exact path="/voyage/application/:id" component={VoyageApplication} />
+          <Route exact path="/weekly/checkin/:id" component={WeeklyCheckin} />
           <Route path="*" exact component={Missing404Page} />
         </Switch>
       </div>
